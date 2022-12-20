e819c17cfd80310a91495c7e9b44c380104a20d6
onarman <onarmannn@gmail.com>
2022-12-20 16:17:45 +0300

    slider done

---
+++ src/data.jsx
@@ -0,0 +1,23 @@
+export const sliderItems =[
+    {
+        id:1,
+        img:"https://img.freepik.com/free-photo/portrait-young-happy-girl-holding-shopping-bags_171337-2608.jpg?w=900&t=st=1671539777~exp=1671540377~hmac=b8272d5442203a57da597069c74f60a975802e07865fae41512f4235f1a074a3",
+        title:"SUMMER SALE",
+        desc:"DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"f5fafd",
+    },
+    {
+        id:2,
+        img:"https://img.freepik.com/free-photo/good-deal-right-corner-confident-pleasant-friendly-looking-african-american-gorgeous-woman-with-afro-haircut-asking-check-out-visit-store-page-pointing-finger-left-smiling-looking-camera_1258-85037.jpg?size=626&ext=jpg&ga=GA1.2.1795913284.1671539768&semt=sph",
+        title:"WINTER SALE",
+        desc:"DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"f5fafd",
+    },
+    {
+        id:3,
+        img:"https://img.freepik.com/free-photo/shocked-two-women-friends-holding-shopping-bags-using-mobile-phone_171337-5718.jpg?size=626&ext=jpg&ga=GA1.1.1795913284.1671539768",
+        title:"POPULAR SALE",
+        desc:"DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"f5fafd",
+    },
+]
